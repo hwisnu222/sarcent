ade4af3b250b611d3a7d9ce0eb2a33bd965a170c
Wisnu Box <hwisnu222@gmail.com>
2026-07-17 12:44:06 +0700

    build: rerun build proto

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main()-> Result<(), Box<dyn std::error::Error>> {
-    // println!("cargo:rerun-if-changed=proto/masterworker.proto");
+    println!("cargo:rerun-if-changed=proto");
 
    tonic_prost_build::compile_protos("proto/storage.proto")?;
    tonic_prost_build::compile_protos("proto/file.proto")?;
